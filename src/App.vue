9e09782a67fc3678ee4b9ce0c789d92e695ee31d
Yerson Reyes <yerson.programador@gmail.com>
2022-06-05 00:51:13 -0400

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header class="mb-4"></header>
+
+    <main>
+      <div class="container">
+        <transition mode="out-in" name="fade">
+          <router-view />
+        </transition>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
+
+<style lang="scss"></style>
